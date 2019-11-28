9bb4ffa9f8e293e4a25d750ec982fdfd0cb89741
jehyun <wpgus2191@gmail.com>
2019-11-28 19:36:26 +0900

    2nd java version

--- DatabaseProject/src/databaseHw.java
+++ DatabaseProject/src/databaseHw.java
@@ -30,9 +30,9 @@ public static void main(String[] args) throws Exception
 		System.out.println(" DATABASE, Computer Science Department ");
 		System.out.println(" at Chungbuk National University ");
 		System.out.println("------------------------------------------------------------");
-		System.out.println(" 1.  2. available zones ");
-		System.out.println(" 3. start instance 4. available regions ");
-		System.out.println(" 5. stop instance 6. create instance ");
+		System.out.println(" 1. 극장별 예매  2. 영화별 예매  ");
+		System.out.println(" 3. 개봉일 보기  4. 시간별 예매 ");
+		System.out.println(" 5. 배우별 출연작 6. create instance ");
 		System.out.println(" 7. reboot instance 8. list images ");
 		System.out.println(" 9. Delete instance 10. Exit ");
 		System.out.println("------------------------------------------------------------");
@@ -42,13 +42,50 @@ public static void main(String[] args) throws Exception
 		
 		switch (number) {
 		case 1:
-			System.out.println("whktjr별 영화검색 : ");
+			MovieList();
+			break;
+		case 2:
+			System.out.println("영화별 예매 : ");
 			instance_id=id_string.next();
 			listCustomer(instance_id);
 			break;
+		case 3:
+			//System.out.println("개봉일 보기 : ");
+			//instance_id=id_string.next();
+			OpeningDate("20191121");
+			break;
+		case 4:
+			System.out.print("시간별 예매 : ");
+			instance_id=id_string.next();
+			Time_res(instance_id);
+		case 5:
+			System.out.print("배우 입력 : ");
+			instance_id=id_string.next();
+			ActorMovielist(instance_id);
 
 		}
 	}
+}
+public static void MovieList() {
+
+	try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection(
+		"jdbc:mysql://192.168.56.105:4567/Moive_res", "jehyun" , "gusdl156");
+		Statement stmt=con.createStatement();
+		
+		StringBuffer sql = new StringBuffer();
+		sql.append("select * from Theater");
+		//sql.append("Select * From Screen where SeatNum <"+ instance_id +" ");
+		ResultSet rs=stmt.executeQuery(sql.toString());
+		System.out.println("Number Movie_name RunningTime Actor OpeningDate");
+		while(rs.next())
+		System.out.println(rs.getInt(1) +" " + rs.getString(3));
+		
+		con.close();
+		
+	}catch(Exception e){ System.out.println(e);}
+	
 }
 public static void listCustomer(String instance_id) {
 	
@@ -60,16 +97,79 @@ public static void listCustomer(String instance_id) {
 		Statement stmt=con.createStatement();
 		
 		StringBuffer sql = new StringBuffer();
-		sql.append("select MovieName from Movie where MovieNum =(select TheaterNum from Screening where MovieNum =(select TheaterNum from Theater where T_Name = \"" + "Chungju_CBNU"+"\" ))");
-		//sql.append("Select * From Screen where SeatNum <"+ instance_id +" ");
+
+		sql.append("Select * From Screen where SeatNum <"+ instance_id +" ");
 		ResultSet rs=stmt.executeQuery(sql.toString());
 		
 		while(rs.next())
-		System.out.println(rs.getString(1));
+		System.out.println(rs.getString(1) +" " + rs.getString(2) +" " + rs.getString(3) +" " + rs.getString(4));
 		
 		con.close();
 		
 	}catch(Exception e){ System.out.println(e);}
+}
+public static void OpeningDate(String instance_id) {
+	try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection(
+		"jdbc:mysql://192.168.56.105:4567/Moive_res", "jehyun" , "gusdl156");
+		Statement stmt=con.createStatement();
+		
+		StringBuffer sql = new StringBuffer();
+
+		sql.append("select MovieNum, MovieName, OpeningDate from Movie");
+		ResultSet rs=stmt.executeQuery(sql.toString());
+		
+		while(rs.next())
+		System.out.println(rs.getString(1) +" " + rs.getString(2)+" " + rs.getString(3));
+		
+		con.close();
+		
+	}catch(Exception e){ System.out.println(e);}
+}
+
+public static void Time_res(String instance_id) {
+
+	try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection(
+		"jdbc:mysql://192.168.56.105:4567/Moive_res", "jehyun" , "gusdl156");
+		Statement stmt=con.createStatement();
+		
+		StringBuffer sql = new StringBuffer();
+		
+		sql.append("select * from Screen where Time= '" + instance_id +" '");
+		ResultSet rs=stmt.executeQuery(sql.toString());
+		
+		while(rs.next())
+		System.out.println(rs.getString(1)+" " + rs.getString(2)+" " + rs.getString(3) +" " + rs.getString(4));
+		
+		con.close();
+		
+	}catch(Exception e){ System.out.println(e);}
+	
+}
+
+public static void ActorMovielist(String instance_id) {
+
+	try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection(
+		"jdbc:mysql://192.168.56.105:4567/Moive_res", "jehyun" , "gusdl156");
+		Statement stmt=con.createStatement();
+		
+		StringBuffer sql = new StringBuffer();
+		
+		sql.append("SELECT * FROM Movie WHERE Actor IN ('" + instance_id + " ') " );
+		ResultSet rs=stmt.executeQuery(sql.toString());
+		
+		while(rs.next())
+		System.out.println(rs.getString(1)+" " + rs.getString(2)+"     " + rs.getString(3) +"     " + rs.getString(4));
+		
+		con.close();
+		
+	}catch(Exception e){ System.out.println(e);}
+	
 }
 
 }
